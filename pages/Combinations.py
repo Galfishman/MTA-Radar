537f2dde8bc92b7132836064ebbf601f9606725f
Galfishman <110173529+Galfishman@users.noreply.github.com>
2023-07-07 14:45:53 +0300

    Update Combinations.py

--- pages/Combinations.py
+++ pages/Combinations.py
@@ -86,7 +86,7 @@ combinations_with_scores.sort(key=lambda x: x[1], reverse=True)
 # Display the selected number of combinations
 st.header("Top Combinations")
 for i, (combination, total_score) in enumerate(combinations_with_scores[:num_combinations]):
-    st.subheader(f"Combination {i+1}")
+    st.subheader('selected_parameter'+f"Combination {i+1}")
     st.write("Players:", ', '.join([f"{player} ({player_data[player]})" for player in combination]))
     st.write("Total Score:", total_score)
     st.write("---")
