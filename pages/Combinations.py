98a9a7ff74b55233710bec0f93afa4e65bcced17
Galfishman <110173529+Galfishman@users.noreply.github.com>
2023-07-10 09:32:36 +0300

    Update Combinations.py

--- pages/Combinations.py
+++ pages/Combinations.py
@@ -6,6 +6,11 @@ import matplotlib as plt
 # Read player data from the CSV file
 df = pd.read_csv('https://raw.githubusercontent.com/Galfishman/MTA-Radar/main/Combinations.csv')
 
+
+# Read player data from the CSV file
+df = pd.read_csv('/Users/galfishman/Desktop/Combinations.csv')
+
+
 df.fillna(0, inplace=True)
 
 # Get unique teams from the 'Team' column
@@ -53,6 +58,7 @@ num_players = st.number_input("Select Number of Players in Combinations", min_va
 # Select number of combinations to display
 num_combinations = st.number_input("Enter Number of Combinations", min_value=1)
 
+
 # Filter data based on selected team and position
 filtered_data = df[df['Team'] == selected_team]
 
@@ -60,6 +66,9 @@ filtered_data = df[df['Team'] == selected_team]
 if selected_position != "All":
     position_filter = filtered_data["Position"].str.contains("|".join(position_options[selected_position]), case=False)
     filtered_data = filtered_data[position_filter.fillna(False)]
+
+# Select players to lock
+players_to_lock = st.sidebar.multiselect("Select Players to Lock", ['None'] + filtered_data['Player'].tolist())
 
 # Select players to exclude
 players_to_exclude = st.sidebar.multiselect("Select Players to Exclude", filtered_data['Player'].tolist())
@@ -75,22 +84,37 @@ player_data = filtered_data.set_index('Player')[selected_parameter].to_dict()
 
 # Find and display the selected number of combinations of players
 combinations_with_scores = []
-for combination in combinations(player_data.keys(), num_players):
-    total_score = sum(player_data[player] for player in combination)
+if "None" in players_to_lock:
+    # Generate combinations without considering locked players
+    combinations_without_locked = combinations(player_data.keys(), num_players)
 
-    if total_score > threshold_score:
-        combinations_with_scores.append((combination, total_score))
+    for combination in combinations_without_locked:
+        total_score = sum(player_data[player] for player in combination)
+
+        if total_score > threshold_score:
+            combinations_with_scores.append((combination, total_score))
+else:
+    for combination in combinations(player_data.keys(), num_players):
+        # Check if all locked players are present in the combination
+        if all(player in combination for player in players_to_lock):
+            total_score = sum(player_data[player] for player in combination)
+
+            if total_score > threshold_score:
+                combinations_with_scores.append((combination, total_score))
+
 
 # Sort combinations by score in descending order
 combinations_with_scores.sort(key=lambda x: x[1], reverse=True)
 
-# Display the selected number of combinations
+
+
 # Display the selected number of combinations
 st.header("Top Combinations")
 for i, (combination, total_score) in enumerate(combinations_with_scores[:num_combinations]):
     st.subheader(f"{selected_parameter} - Combination {i+1}")
-    st.write("Players:", ', '.join([f"<span style='color: white;'>{player} ({player_data[player]})</span>" for player in combination]), unsafe_allow_html=True)
-    formatted_score = "{:.3f}".format(total_score)
-    st.write("Total Score:", f"<span style='color: yellow;'>{formatted_score}</span>", unsafe_allow_html=True)
+    st.write("Players:", ', '.join([f"<b>{player} (<span style='color: yellow;'>{player_data[player]:.3f}</span>)</b>" for player in combination]), unsafe_allow_html=True)
+    formatted_score = "<h2><span style='color: yellow;'>{:.3f}</span></h2>".format(total_score)
+    st.write("<span style='font-size: 22px;'>Total Score:</span>", f"<span style='color: yellow; font-size: 22px; font-weight: bold;'>{total_score:.3f}</span>", unsafe_allow_html=True)
     st.write("---")
+
 
