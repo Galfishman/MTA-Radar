654f7ef7d2b2b661ed1ef98bb3b3b2961ce8b3e5
Galfishman <110173529+Galfishman@users.noreply.github.com>
2024-02-18 23:20:58 +0200

    Update Radar.py

--- Radar.py
+++ Radar.py
@@ -4,103 +4,53 @@ import streamlit as st
 from soccerplots.radar_chart import Radar
 from mplsoccer import PyPizza
 import matplotlib as mpl
-import pickle
-from pathlib import Path
 
 ###login
+st.title("MTA RADAR Comparison")
 
 # READ DATA
 @st.cache_data  # Cache the result of this function
 def load_data():
     return pd.read_csv('https://raw.githubusercontent.com/Galfishman/MTA-Radar/main/Players%20Export.csv')
 
-rawdf = load_data()
-
-
-st.title("MTA Radar Compression")
-# READ DATA
-#rawdf = pd.read_csv('https://raw.githubusercontent.com/Galfishman/MTA-Radar/main/DataBase.csv')
-
+# Define the mapping of Short Position to Position 1
+position_mapping = {
+    'Center Backs': ['Right Centre Back', 'Left Centre Back', 'Central Defender'],
+    'Full Backs': ['Right Wing Back', 'Right Back', 'Left Wing Back', 'Left Back'],
+    'Midfielders': ['Right Midfielder', 'Left Midfielder', 'Defensive Midfielder', 'Centre Attacking Midfielder', 'Central Midfielder'],
+    'Wingers': ['Right Winger', 'Right Attacking Midfielder', 'Left Winger', 'Left Attacking Midfielder'],
+    'Strikers': ['Second Striker', 'Centre Forward'],
+    'GK': ['Goalkeeper'],
+}
 
 st.sidebar.header("Please Filter Here:")
 
-min_selection = st.sidebar.slider('Minutes played:',
-                                  min_value=int(rawdf['Minutes played'].min()),
-                                  max_value=int(rawdf['Minutes played'].max()),
-                                  value=(int(rawdf['Minutes played'].min()), int(rawdf['Minutes played'].max())))
-filtered_df = rawdf[(rawdf['Minutes played'] >= min_selection[0]) & (rawdf['Minutes played'] <= min_selection[1])]
+# Filter by position group
+selected_position_group = st.sidebar.selectbox(
+    "Filter by Position Group:",
+    options=list(position_mapping.keys()),
+)
 
 
-position_options = {
-    "All": "",
-    "Wingers": ["rw", "lw", "lam", "ram"],
-    "Strikers": ["cf", "st"],
-    "Midfielders": ["cmf", "dmf", "rcmf", "lcmf", "amf","LCMF"],
-    "Defenders": ["rb", "cb", "lb", "rcb", "lcb","rwb","lwb"],
-}
 
-selected_position = st.sidebar.selectbox("Select Position:", options=list(position_options.keys()))
+# Filter the DataFrame based on the selected position group
+filtered_players = df[df['pos'].isin(position_mapping[selected_position_group])]
 
-# Apply position filtering
-if selected_position != "All":
-    position_filter = rawdf["Position"].str.contains("|".join(position_options[selected_position]), case=False)
-    filtered_df = rawdf[position_filter.fillna(False)]
-else:
-    filtered_df = rawdf
+# List of players based on the selected position group
+players_list = filtered_players["Player"].unique()
 
-df = filtered_df
-
-# Display the filtered DataFrame
-
-# Create or retrieve session state
-if 'state' not in st.session_state:
-    st.session_state.state = {'selected_leagues': [], 'selected_player': None, 'Name': None, 'Name2': None}
-
-# Filter by league
-all_leagues = df["League"].unique()
-all_leagues = ['All'] + all_leagues.tolist()
-selected_leagues = st.multiselect("Select League:", options=all_leagues, default=["All"])
-
-# Store selected leagues in session state
-st.session_state.state['selected_leagues'] = selected_leagues
-
-if "All" in selected_leagues:
-    df = df  # No filtering needed if "All" is selected
-else:
-    df = df[df["League"].isin(selected_leagues)]
-
-# Retrieve or initialize Name and Name2 from session state
-Name = st.session_state.state.get('Name')
-Name2 = st.session_state.state.get('Name2')
-
-# Select the Player
-player_options = df["Player"].unique().tolist()
-player_options2 = ["League Average"] + df["Player"].unique().tolist()
 Name = st.sidebar.selectbox(
     "Select the Player:",
-    options=player_options,
-    index=player_options.index(Name) if Name in player_options else 0,
-    key='NameSelector',
+    options=players_list,
 )
+
 Name2 = st.sidebar.selectbox(
     "Select other Player:",
-    options=player_options2,
-    index=player_options2.index(Name2) if Name2 in player_options2 else 0,
-    key='Name2Selector',
+    options=["League Average"] +filtered_players["Player"].unique().tolist(),
 )
 
-# Store selected player in session state
-st.session_state.state['Name'] = Name
-st.session_state.state['Name2'] = Name2
 
-# Rest of the code remains the same...
-
-
-# Create a collapsible section for the DataFrame
-with st.expander("Show Players Table"):
-    # Display the DataFrame
-    st.dataframe(df)
-
+    
 # List of all available parameters
 all_params = list(df.columns[7:])
 
@@ -108,10 +58,15 @@ all_params = list(df.columns[7:])
 selected_params = st.sidebar.multiselect(
     "Select Parameters:",
     options=all_params,
-    default=("Key passes per 90", "Dribbles per 90", "Passes to penalty area per 90", "Shot assists per 90", "Crosses per 90", "Shots per 90", "xG per 90")  # Default value is all_params (all parameters selected)
-)
+    default=("xG", "TchsA3", "TouchOpBox", "SucflDuels", "Success1v1"))  # Default value is all_params (all parameters selected)
 
 params = selected_params
+
+with st.expander("Show Players Table"):
+    # Display the DataFrame with only selected parameters
+    selected_columns = ['Player','Team','Min'] + selected_params
+    st.dataframe(filtered_players[selected_columns])
+
 
 # add ranges to list of tuple pairs
 ranges = []
@@ -133,23 +88,19 @@ for _, row in df.iterrows():
     if row['Player'] == Name2:
         b_values = row[params].tolist()
 
-# Find the minutes played for the selected players
-minutes_player1 = df.loc[df['Player'] == Name, 'Minutes played'].values[0]
 
-league_player2 = ""
 if Name2 == "League Average":
-    league_average_values = df[df['League'] == df.loc[df['Player'] == Name, 'League'].values[0]][params].mean().tolist()
+    league_average_values = filtered_players[filtered_players['Player'] != Name][params].mean().tolist()
     b_values = league_average_values
     title_name2 = "League Average"
 else:
     player2_row = df[df['Player'] == Name2]
     if not player2_row.empty:
-        minutes_player2 = player2_row['Minutes played'].values[0]
-        Position_name2 = player2_row['Position'].values[0]
+        minutes_player2 = player2_row['Min'].values[0]
+        Position_name2 = player2_row['pos'].values[0]
         team_name2 = player2_row['Team'].values[0]
-        league_player2 = player2_row['League'].values[0]
         b_values = player2_row[params].values[0].tolist()
-        title_name2 = f"{Name2}\n{'Position: ' + Position_name2}\n{'Team:  ' + team_name2}\n{'League: ' + league_player2}\n{minutes_player2} Minutes Played"
+        title_name2 = f"{Name2}\n{'Position: ' + Position_name2}\n{'Team:  ' + team_name2}\n{minutes_player2} Minutes Played"
     else:
         st.error(f"No data available for player: {Name2}")
         st.stop()
@@ -158,13 +109,23 @@ a_values = a_values[:]
 b_values = b_values[:]
 values = [a_values, b_values]
 
+# Print values for troubleshooting
+minutes_name = "Min"
+minutes_player1 = filtered_players.loc[filtered_players['Player'] == Name, minutes_name].values[0]
+
+Position_name = "pos"
+Position_name1 = filtered_players.loc[filtered_players['Player'] == Name, Position_name].values[0]
+
+Team_name = "Team"
+team_name1 = filtered_players.loc[filtered_players['Player'] == Name, Team_name].values[0]
+
 # Update the title dictionary with minutes played
 title = dict(
-    title_name=f"{Name}\n{'Position: ' + df.loc[df['Player'] == Name, 'Position'].values[0]}\n{'Team:  ' + df.loc[df['Player'] == Name, 'Team'].values[0]}\n{'League: ' + df.loc[df['Player'] == Name, 'League'].values[0]}\n{minutes_player1} Minutes Played",
+    title_name=f"{Name}\n{'Team: ' + team_name1}\n{Position_name1}\n{minutes_player1} Minutes Played",
     title_color='yellow',
-    title_name_2=title_name2,
+    title_name_2= title_name2,
     title_color_2='blue',
-    title_fontsize=13,
+    title_fontsize=12,
 )
 
 # RADAR PLOT
@@ -172,7 +133,7 @@ radar = Radar(
     background_color="#121212",
     patch_color="#28252C",
     label_color="#FFFFFF",
-    label_fontsize=9,
+    label_fontsize=10,
     range_color="#FFFFFF"
 )
 
@@ -190,6 +151,23 @@ fig, ax = radar.plot_radar(
     compare=True
 )
 
-mpl.rcParams['figure.dpi'] = 2500
+mpl.rcParams['figure.dpi'] = 2400
 
 st.pyplot(fig)
+
+head_to_head_df = pd.DataFrame({
+    'Player': [Name, Name2],
+    **{param: [a_values[i], b_values[i]] for i, param in enumerate(params)}
+})
+
+# Transpose the DataFrame to have parameters as rows
+head_to_head_df_transposed = head_to_head_df.set_index('Player').T
+
+# Identify the highest value for each parameter across both players
+max_values = head_to_head_df_transposed.max()
+
+# Highlight the highest value in each row across both players
+highlighted_df = head_to_head_df_transposed.style.format("{:.2f}").apply(lambda row: ['background-color: grey' if val == row.max() else '' for val in row], axis=1)
+
+st.header("Head-to-Head Comparison")
+st.table(highlighted_df)
